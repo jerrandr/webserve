04dbbbe188e4f647a8277b4135b4edc0e72e8e1b
Rova Andrinantenaina <randrina@c1r13s1.42antananarivo.mg>
2025-08-04 16:01:14 +0300

    rova push

--- config_parsing.cpp
+++ config_parsing.cpp
@@ -12,12 +12,40 @@
 
 #include "Server.hpp"
 
+static int      content_error_handling(std::string line)
+{
+    std::string all_keys[13] = {"server", "host", "listen", "error_page",
+         "client_max_body_size", "local", "method", "root", "autoindex",
+          "index", "route", "upload", "cgi"};
+    /*int t = all_keys->find(line);
+    std::cout << "content erro_handling" << all_keys[t] << std::endl;
+    return (0);*/
+    (void) line;
+    return (1);
+};
+
+static void     line_parsing(std::string line)
+{
+    int             last;
+    size_t          i;
+    std::string     key_w;
+    std::string     new_line;
+    i = 0;
+    while (isspace(line[i]))
+        i ++;
+    new_line = line.substr(i, line.size() - i);
+    last = new_line.find(" ");
+    key_w = new_line.substr(0, last);
+    std::cout << key_w << std::endl;
+};
+
 static void     get_all_line(std::string input)
 {
-    int         len;
-    size_t      pos;
-    std::string line;
-    std::string new_input;
+    int                                 len;
+    size_t                              pos;
+    std::string                         line;
+    std::string                         new_input;
+    std::vector<std::string>            key_vect;
 
     pos = 0;
     new_input = input;
@@ -25,10 +53,13 @@ static void     get_all_line(std::string input)
     {
         len = new_input.find("\n");
         line = input.substr(pos,len);
-        pos += len+1;
+        line_parsing(line);
+        if (len == -1)
+            break ;
+        pos += len + 1;
         new_input = input.substr(pos,input.size() - pos);
-        std::cout << new_input << std::endl;
     };
+    content_error_handling("anana");
 };
 
 void    config_parsing(int fd)
