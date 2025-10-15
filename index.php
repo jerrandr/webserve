4a7ff876a724a60afa9407d846da780bc189a358
Marinah Salohy <msalohy@c1r1s5.42antananarivo.mg>
2025-10-15 12:34:18 +0300

    removes files

--- index.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-    echo "
-          <form method=\"post\" enctype=\"multipart/form-data\" >
-            <input type=\"file\" name=\"monfichier\" required />
-            <button type=\"submit\">OK</button>
-        </form>
-    ";
-?>
--- www/test.php
+++ www/test.php
@@ -1,5 +1,5 @@
 <?php
     echo "<h1>BEGIN</h1>";
-    sleep(30);
+    sleep(100);
     echo "<h1>END</h1>";
 ?>
